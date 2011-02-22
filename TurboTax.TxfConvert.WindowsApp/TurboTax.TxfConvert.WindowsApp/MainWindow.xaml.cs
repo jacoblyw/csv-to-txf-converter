0399e3f575c06db55049f71481fd0598a0f9956d
contact alexkuo <alexkuo@gmail.com>
2011-02-22 16:48:07 -0600

    Initial Commit

---
+++ TurboTax.TxfConvert.WindowsApp/TurboTax.TxfConvert.WindowsApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurboTax.TxfConvert.WindowsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertToTXF_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextboxCsvFile.Text)
+                && !string.IsNullOrEmpty(TextboxOutputDirectory.Text))
+            {
+                try
+                {
+                    TXFConverter txf = new TXFConverter(TextboxCsvFile.Text, TextboxOutputDirectory.Text);
+                    txf.Execute();
+
+                    if (!string.IsNullOrEmpty(txf.OutputFile))
+                    {
+                        MessageBox.Show(string.Concat("File operation complete! File saved at: ", txf.OutputFile));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                
+            }
+        }
+    }
+}
